3ce7ed7d6456752539d4f08970d99ed48b37a885
Sanchez-9E-ay2526 <sanchez.bmd@obmontessori.edu.ph>
2026-04-17 11:01:01 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function openGarage(){
+let carCounter = 18;
+const maxCars =25;
+
+while (carCounter < maxCars){
+window.alert ("Car Number" + carCounter + "is entering")
+carCounter += 1;
+
+}
+window.alert(Garage is full);
+}
